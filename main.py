5c9609026357aee8d4518f5f05d9273e32678075
LouisEsteve <105226352+LouisEsteve@users.noreply.github.com>
2022-10-12 21:35:55 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,122 @@
+import re
+import time
+import pandas as pd
+
+#######################################
+
+encoding	=	'UTF-8'
+data_path	=	'data'
+
+data_dict	=	{
+					'NER_TRAIN_PREAMBLE'	:	{
+						'csv_path'			:	f'{data_path}/NER_TRAIN_PREAMBLE.csv',
+						'regex_patterns'	:	{
+							'COURT'	:	[
+								# '[hH]igh\\s[cC]ourt\\s[oO]f\\s[A-Z].*?\\b',
+								'([hH]igh|[sS]upreme)\\s[cC]ourt\\s[oO]f\\s[A-Z].*?\\b[aA]t( [A-Z].*?\\b)+',
+								# '([hH]igh|[sS]upreme)\\s[cC]ourt\\s[oO]f\\s[A-Z]\\S*(\\s[aA]t( [A-Z].*?\\b)+)?',
+								# '([hH]igh|[sS]upreme)\\s[cC]ourt\\s[oO]f( [A-Z]\\S*)+( [aA]t( [A-Z].*?\\b)+)?',
+								# '([hH]igh|[sS]upreme)\\s+[cC]ourt\\s+[oO]f( [A-Z][^\\n \\t\\r,\\.]*)+(\\s+[aA]t(\\s+[A-Z].*?\\b)+)?',
+								# '([hH]igh|[sS]upreme)\\s+[cC]ourt\\s+[oO]f( [A-Z][^\\n \\t]*)+(\\s+[aA]t(\\s+[A-Z].*?\\b)+)?',
+								# '([hH]igh|[sS]upreme)\\s+[cC]ourt\\s+[oO]f( [A-Z].*?\\b)+(\\s+[aA]t([ \\t]+[A-Z].*?\\b)+)?',
+								# '([hH]igh|[sS]upreme)\\s+[cC]ourt\\s+[oO]f( [A-Z].*?\\b)+(\\s+[aA]t\\n?([ \\t]*[A-Z].*?\\b)+)?',
+								# '([hH]igh|[sS]upreme)\\s+[cC]ourt\\s+[oO]f( [A-Z].*?\\b)+(\\s+[aA]t\n?([ \\t]*[A-Z].*?\\b)+)?',
+								# '([hH]igh|[sS]upreme)\\s+[cC]ourt\\s+[oO]f( [A-Za-z].*?\\b)+(\\s+[aA]t\n?([ \\t]*[A-Z].*?\\b)+)?',
+								# '(([hH]igh|[sS]upreme)\\s+)?[cC]ourt\\s+[oO]f( [A-Za-z].*?\\b)+(,?\\s+[aA]t\n?([ \\t]*[A-Z].*?\\b)+)?',
+								# '([hH]igh|[sS]upreme)\\s+[cC]ourt\\s+[oO]f( [A-Za-z].*?\\b)+(,?\\s+[aA]t\n?([ \\t]*[A-Z].*?\\b)+)?',
+								# '([hH]igh|[sS]upreme)\\s+[cC]ourt\\s+[oO]f(( [A-Za-z].*?\\b)|\\s?[:;])+(,?\\s+[aA]t\n?([ \\t]*[A-Z].*?\\b)+)?',
+								# '([hH]igh|[sS]upreme)\\s+[cC]ourt\\s+[oO]f(( [A-Za-z][^\\n ]*\\b)|\\s?[:;])+(,?\\s+[aA]t\n?([ \\t]*[A-Z].*?\\b)+)?',	#0.84, 0.59
+								# '([hH]igh|[sS]upreme)\\s+[cC]ourt\\s+[oO]f(( [A-Za-z][^\\n ]*\\b)|\\s?[:;])+(,?\\s+[aA]t\n?(,?[ \\t]*[A-Z].*?\\b)+)?',
+								# '([hH]igh|[sS]upreme)\\s+[cC]ourt\\s+[oO]f(( [A-Za-z][^\\n ]*\\b)|\\s?[:;\\.])+(,?\\s+[aA]t\n?(,?[ \\t]*[A-Z].*?\\b)+)?',
+								# '([hH]igh|[sS]upreme)\\s+[cC]ourt\\s+[oO]f(( [A-Za-z][^\\n ]*\\b)|\\s?[:;\\.])+,?(\\s+[aA]t\n?(,?[ \\t]*[A-Z].*?\\b)+)?',
+								# '([hH]igh|[sS]upreme)\\s+[cC]ourt\\s+[oO]f(( [A-Za-z][^\\n ]*\\b|\\s?[:;\\.]),?)+(\\s+[aA]t\n?(,?[ \\t]*[A-Z].*?\\b)+)?',	#0.85, 0.59
+								# '([hH]igh|[sS]upreme)\\s+[cC]ourt\\s+[oO]f(,?( [A-Za-z][^\\n ]*\\b|\\s?[:;\\.]))+(\\s+[aA]t\n?(,?[ \\t]*[A-Z].*?\\b)+)?',	#0.86, 0.60
+								# '([hH]igh|[sS]upreme)\\s+[cC]ourt\\s+[oO]f(,?( [A-Za-z][^\\n ]*\\b|\\s?[:;\\.]))+(\\s+[aA]t\\n?(,?[ \\t]*[A-Z].*?\\b)+)?',	#0.86, 0.60
+								# '([hH]igh|[sS]upreme)\\s+[cC]ourt\\s+[oO]f(,?( [A-Za-z][^\\n ]*\\b|\\s?[:;\\.]))+(\\s+[aA]t\\n?[^\\n]+)?',	#0.86, 0.60
+								# '([hH]igh|[sS]upreme)\\s+[cC]ourt\\s+[oO]f(,?( [A-Za-z][^\\n ]*\\b|\\s?[:;\\.]))+(\\s+[aA]t\\n{0,1}[^\\n]+)?',	#0.86, 0.60
+								'([A-Z].*?\\b [bB]ench [oO]f [A-Z].*?\\b )?([hH]igh|[sS]upreme)\\s+[cC]ourt\\s+[oO]f(,?( [A-Za-z][^\\n ]*\\b|\\s?[:;\\.]))+(\\s+[aA]t\\n{0,1}[^\\n]+)?',	#0.86, 0.60
+								# '(?<=[iI]n [tT]he )([A-Z]\\S* )*[cC]ourts?( [A-Z][^\\n ]*)*',
+								'(?<=[iI]n [tT]he )([A-Z]\\S* )*([cC]ourt|[tT]ribunal|[sS]ession)s?( [A-Z][^\n ]*)*',	#0.61, 0.52
+								# '([A-Z].*?\\b [bB]ench [oO]f [A-Z].*?\\b )?([hH]igh|[sS]upreme)\\s+([cC]ourt|[tT]ribunal|[sS]ession)s?( [A-Z][^\n ]*)*',	#0.70, 0.56
+								# '([A-Z].*?\\b [bB]ench [oO]f [A-Z].*?\\b )?(([hH]igh|[sS]upreme)\\s+)?([cC]ourt|[tT]ribunal|[sS]ession)s?( [A-Z][^\n ]*)*(,?\\n[^\\n]+)?',	#0.29, 0.50
+							]
+						}
+					}
+				}
+
+#######################################
+
+def main() -> int:
+	text_base_path	=	f'{data_path}/text_base.csv'
+	text_base_file	=	open(text_base_path, 'rt', encoding=encoding)
+	text_base_df	=	pd.read_csv(text_base_file, sep='\t')
+	text_base_df['text']	=	text_base_df['text'].str.replace('\\n','\n',regex=False)
+	text_base_df['text']	=	text_base_df['text'].str.replace('\\t','\t',regex=False)
+	text_base_df['text']	=	text_base_df['text'].str.replace('\\r','\r',regex=False)
+	print(text_base_df)
+	print(f'Loaded {text_base_path}')
+	
+	###################################################
+	
+	for i in data_dict:
+		print(f'Starting {i} with CSV from path {data_dict[i]["csv_path"]}')
+		
+		set_file	=	open(data_dict[i]['csv_path'], 'rt', encoding=encoding)
+		set_df		=	pd.read_csv(set_file, sep='\t')
+		
+		for j in data_dict[i]['regex_patterns']:
+			print(f'Starting {j}')
+			
+			for k in data_dict[i]['regex_patterns'][j]:
+				# print(f'Compiling {k}')
+				local_regex				=	re.compile(k)
+				
+				count_true_positive		=	0
+				count_false_positive	=	0
+				count_false_negative	=	0
+				
+				for m in text_base_df['id']:
+					if set_df[set_df['text_id'] == m].shape[0] == 0:
+						continue
+					
+					local_text					=	text_base_df['text'][text_base_df.index[text_base_df['id'] == m].tolist()[0]]
+					
+					local_count_true_positive	=	0
+					local_count_false_positive	=	0
+					local_count_false_negative	=	0
+				
+					regex_result				=	local_regex.finditer(local_text)
+					
+					expected_results			=	set_df.loc[(set_df['annotation_label'] == j) & (set_df['text_id'] == m)]
+					
+					for n in regex_result:
+						start	=	n.start()
+						end		=	n.end()
+						# print(n)
+						
+						if expected_results.loc[(expected_results['annotation_start'] == start) & (expected_results['annotation_end'] == end)].shape[0] > 0:
+							local_count_true_positive	+=	1
+						else:
+							# print(f'False positive: {n}')
+							local_count_false_positive	+=	1
+					
+					local_count_false_negative	=	expected_results.shape[0] - local_count_true_positive
+					
+					count_true_positive			+=	local_count_true_positive
+					count_false_positive		+=	local_count_false_positive
+					count_false_negative		+=	local_count_false_negative
+				
+				# print(f'{k}\t{count_true_positive}\t{count_false_positive}\t{count_false_negative}')
+				print(f'{k}\t{count_true_positive/(count_true_positive+count_false_positive)}\t{count_true_positive/(count_true_positive+count_false_negative)}')
+		
+		set_file.close()
+	
+	###################################################
+	
+	text_base_file.close()
+	return 0
+
+if __name__ ==	'__main__':
+	t		=	time.time()
+	result	=	main()
+	print(f'Program ended with status {str(result)} in {str(time.time()-t)}s')
---
+++ parser1.py
@@ -0,0 +1,158 @@
+import json
+from os import listdir
+from os.path import exists
+import re
+
+data_path					=	'data'
+
+encoding					=	'UTF-8'
+
+#sentence_pattern    	 	 =   '(?s)[A-Z][^((?<![A-Z][A-Za-z]*)\\.)\\?!]*'
+#sentence_regex      	 	 =   re.compile(sentence_pattern)
+
+csv_separator       		=   '\t'
+csv_replacements    		=   [
+    ['\t', '\\t'],
+    ['\n', '\\n'],
+    ['\r', '\\r']
+]
+
+tab							=	'\t'
+tab_replacement				=	'\\t'
+linefeed					=	'\n'
+linefeed_replacement		=	'\\n'
+carriage_return				=	'\r'
+carriage_return_replacement	=	'\\r'
+
+text_dict					=	{}
+
+def main():
+    files = listdir(data_path)
+    for i in files:
+        #if (not i.endswith('.json')) or (not i.startswith('RR')):
+        if not i.endswith('.json'):
+            continue
+        local_path = f'{data_path}/{i}'
+        print(local_path)
+        try:
+            local_file = open(local_path, 'rt', encoding='utf-8')
+        except IOError or OSError as m:
+            print(m)
+        else:
+            sum_for_average     =   0
+            count_for_average   =   0
+            
+            json_object = json.load(local_file)
+            #print(dir(json_object))
+
+            str_for_csv = csv_separator.join(
+                    [
+                        'text_id',
+                        # 'text',
+                        'text_group',
+                        'text_source',
+                        'annotation_result_group',
+                        'annotation_id',
+                        'annotation_type',
+                        'annotation_to_name',
+                        'annotation_from_name',
+                        'annotation_start',
+                        'annotation_end',
+                        'annotation_text',
+                        'annotation_label'
+                    ]
+            )
+
+            j_count = 1
+            for j in json_object:
+                #local_sentences = sentence_regex.findall(j.data.text)
+                
+                print(f'Parsing text {j_count}/{len(json_object)}...', end='\r')
+				
+                text_id                         =   j['id']             if j['id'] else hash(j['data']['text'])    			#j.id
+                
+                # if j['id'] not in text_dict:
+                if text_id not in text_dict:
+                    # text_dict[j['id']] = j['data']['text']
+                    text_dict[text_id] = j['data']['text']
+                
+                sum_for_average                 +=  len(j['data']['text'])
+                count_for_average               +=  1
+                
+
+                # text_id                         =   j['id']             if len(j['id']) > 0 else hash(j['data']['text'])    #j.id
+                # text                          =   j['data']['text']                                                       #j.data.text
+                text_group                      =   j['meta']['group']  if 'group' in j['meta'].keys() else '_'             #j.meta.group
+                text_source                     =   j['meta']['source'] if 'source' in j['meta'].keys() else '_'            #j.meta.group
+
+                for q in range(len(j['annotations'])):
+                    annotation_result_group     =    q
+                    
+                    #for k in j['annotations'][0]['result']:
+                    for k in j['annotations'][q]['result']:
+                        annotation_id           =   k['id']                                                                 #k.id
+                        annotation_type         =   k['type']           if 'type' in k.keys() else '_'                      #k.type
+                        annotation_to_name      =   k['to_name']        if 'to_name' in k.keys() else '_'                   #k.to_name
+                        annotation_from_name    =   k['from_name']      if 'from_name' in k.keys() else '_'                 #k.from_name
+                        annotation_start        =   k['value']['start']                                                     #k.value.start
+                        annotation_end          =   k['value']['end']                                                       #k.value.end
+                        annotation_text         =   k['value']['text']                                                      #k.value.text
+
+                        for m in k['value']['labels']:
+                            annotation_label    =   m
+                            
+                            cell_values         =   [
+                                str(text_id),
+                                # str(text),
+                                str(text_group),
+                                str(text_source),
+                                str(annotation_result_group),
+                                str(annotation_id),
+                                str(annotation_type),
+                                str(annotation_to_name),
+                                str(annotation_from_name),
+                                str(annotation_start),
+                                str(annotation_end),
+                                str(annotation_text),
+                                str(annotation_label)
+                            ]
+                            
+                            
+                            #for n in cell_values:
+                            #    for p in csv_replacements:
+                            #        n           =   n.replace(p[0], p[1])
+
+                            for n in range(len(cell_values)):
+                                for p in csv_replacements:
+                                    cell_values[n]  =   cell_values[n].replace(p[0], p[1])
+
+                            local_line_for_csv  =   csv_separator.join(cell_values)
+
+                            str_for_csv         =   f'{str_for_csv}\n{local_line_for_csv}'
+                j_count += 1
+            local_file.close()
+            print('')
+            
+            output_count = 0
+            output_path = f'{local_path[:-5]}_restructured{str(output_count)}.csv'
+            while(exists(output_path)):
+                output_count += 1
+                output_path = f'{local_path[:-5]}_restructured{str(output_count)}.csv'
+            f = open(output_path, 'wt', encoding='utf-8')
+            f.write(str_for_csv)
+            f.close()
+            print(f'Wrote {output_path} (character count: {len(str_for_csv)})')
+            print(f'Average length: {sum_for_average/count_for_average}\n{"-"*32}')
+            #break
+    text_base_path  =   f'{data_path}/text_base.csv'
+    f               =   open(text_base_path,'wt',encoding=encoding)
+    # s               =   '\n'.join(['\t'.join([i,text_dict[i].replace('\t','\\t').replace('\n','\\n') for i in text_dict])])
+    s               =   'id\ttext\n' + '\n'.join([f"{i}\t{text_dict[i].replace(tab,tab_replacement).replace(linefeed,linefeed_replacement).replace(carriage_return,carriage_return_replacement)}" for i in text_dict])
+    f.write(s)
+    print(f'Wrote {text_base_path}')
+    f.close()
+
+
+if __name__ == '__main__':
+    main()
+
