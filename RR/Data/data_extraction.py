8a3ef0a71935ca9ddb3358b3d5e5ed6222210d59
jokkloo <jokkloo@outlook.com>
2023-01-03 13:31:45 +0100

    Organisation

---
+++ RR/Data/data_extraction.py
@@ -0,0 +1,51 @@
+# LIBRAIRIES
+import os
+import sys
+import re
+
+from datetime import datetime
+import json
+import pandas as pd
+
+# DATA
+CUR_DIR = os.path.dirname(os.path.realpath(sys.argv[0]))
+BASE_DIR = os.path.dirname(CUR_DIR)
+DATA_DIR = BASE_DIR + os.sep + "Data"
+files = [file for file in os.listdir(DATA_DIR)]
+
+
+# CODE
+for file in files:
+    root, ext = os.path.splitext(file)
+    if ext==".json":
+        print(f"Fichier en cours de traitement : {file}...\n")
+        f = open(BASE_DIR + os.sep + "Data" + os.sep + file)
+        data = json.load(f)
+        
+        now = datetime.now()
+        time_full = now.strftime("%Y%m%d %H%M%S ")
+
+        df_ = []
+        for i, item in enumerate(data):
+            id = item["id"]
+            full_text = item["data"]["text"]
+            meta_info = item["meta"]["group"]
+            annotations = item["annotations"]
+            for i in annotations:
+                result = i["result"]
+                for i, item in enumerate(result):
+                    text_id = item["id"]
+                    text = item["value"]["text"]
+                    text = re.sub("\n", "", text)
+                    label = item["value"]["labels"][0]
+                    df_.append([id, text_id, text, label, full_text, meta_info])
+
+        df = pd.DataFrame(df_, columns=["doc_id", "text_id", "text", "label", "full_text", "meta"])
+        print(df.tail())
+        print("\n")
+        filename = DATA_DIR + os.sep + root + ".csv"
+        # filename = DATA_DIR + time_full + root + ".csv"
+        df.to_csv(filename, sep=";", index=False)
+        print(f"Fichier de sortie produit : {filename}\n")
+
+print("Fin du traitement.")
--- RR/ngrams/ngrams.py
+++ RR/ngrams/ngrams.py
@@ -1,1 +1,61 @@
+# LIBRAIRIES
+import os
+import sys
+from collections import defaultdict
+
+import spacy
+nlp = spacy.load("en_core_web_sm")
+
 import pandas as pd
+
+
+# DATA -- A paramétrer si besoin
+CUR_DIR = os.path.dirname(os.path.realpath(sys.argv[0]))
+BASE_DIR = os.path.dirname(CUR_DIR)
+DATA_DIR = BASE_DIR + os.sep + "Data"
+files = [file for file in os.listdir(DATA_DIR) if ".csv" in file]
+for file in files:
+    if file=="train.csv":
+        TRAIN = DATA_DIR + os.sep + file
+    elif file=="dev.csv":
+        DEV = DATA_DIR + os.sep + file
+
+
+stopwords = nlp.Defaults.stop_words
+
+# FONCTIONS
+def process_text(text):
+    text = text.lower()
+    doc = nlp(text)
+    lemmas = [token.lemma_ for token in doc if token.lemma_ not in set(stopwords) and token.lemma_!=""]
+    return lemmas
+
+# def get_ngrams(text, n):
+#     """
+#     Découper un texte en tokens, enlever les stopwords et renvoyer des n-grams. N à désigner.
+#     """
+#     temp = zip(*[token[i:] for i in range(0, n)])
+#     ngrams = [' '.join(ngram) for ngram in temp]
+#     return ngrams
+
+# def count_ngrams(list_obj):
+#     ngram_counts = {x:defaultdict(lambda: 0) for x in RR_types}
+    
+#     for label, ddict in ngram_counts.items():
+#         # print('Processing:', label)
+#         if list_obj:
+#             for item in list_obj:
+#                 ddict[item] += 1
+#     return ngram_counts
+
+
+# CODE
+df = pd.read_csv(TRAIN, sep=";", encoding="UTF-8")
+print(f"En cours de traitement : {TRAIN}\n")
+# print(df_TRAIN.head(10))
+
+df["lemmatised"] = df["text"].apply(process_text)
+
+print(df[["text", "lemmatised"]].head(10))
+
+
