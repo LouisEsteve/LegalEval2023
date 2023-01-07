0a9f06a25e47242a56c7a0b2ea4190130ccb3b0b
sebtl <112858392+sebtl@users.noreply.github.com>
2023-01-07 14:39:26 +0100

    Update analyse.py

--- RR/bow/analyse.py
+++ RR/bow/analyse.py
@@ -5,21 +5,31 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
+
+# OUVERTURE DU FICHIER
 df = pd.read_csv('prediction.csv',sep='\t')
 
 y_true  =   df['annotation_label']
 y_pred  =   df['predicted_label']
 
 
+# RAPPORT DE CLASSIFICATION
 classification_report = classification_report(y_true,y_pred)
 print(classification_report)
 
-confusion_matrix = pd.crosstab(y_true, y_pred, rownames=['Actual'], colnames=['Predicted'])
 
+# MATRICE DE CONFUSION
+confusion_matrix = pd.crosstab(y_true, y_pred, rownames=['Actual'], colnames=['Predicted'])
 print(confusion_matrix)
 
-sns.heatmap(confusion_matrix, annot=True, fmt='g', cmap="OrRd")
 
+# AFFICHER LA MATRCIE DE CONFUSION PNG
+sns.heatmap(confusion_matrix, annot=True, fmt='g', cmap="OrRd")
 plt.ylabel('Valeurs réelles')
 plt.xlabel('Valeurs prédites')
-plt.show()
+#plt.show()
+
+# CALCULER l'OVERLAP
+overlap = pd.crosstab(df['annotation_label'], df['predicted_label']).apply(lambda x: x[x.index != x.name].sum(), axis=1)
+overlap = overlap.sort_values(ascending=False)
+print(overlap)
