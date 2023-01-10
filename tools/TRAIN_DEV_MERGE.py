cc12cf4ba0f065a767f55e8719f58c9ffd092666
LouisEsteve <105226352+LouisEsteve@users.noreply.github.com>
2023-01-10 16:59:19 +0100

    Config file for TRAIN_DEV_MERGE.py

--- tools/TRAIN_DEV_MERGE.py
+++ tools/TRAIN_DEV_MERGE.py
@@ -1,47 +1,58 @@
 import pandas as pd
+import json
 
-TEXT_BASE_TRAIN_PATH		=	"data/NER_TRAIN_text_base.csv"
-TEXT_BASE_DEV_PATH		=	"NER_DEV/NER_DEV/NER_DEV_text_base.csv"
-ANNOT_PREAMBLE_TRAIN_PATH	=	"data/NER_TRAIN_PREAMBLE_restructured7.csv"
-ANNOT_JUDGEMENT_TRAIN_PATH	=	"data/NER_TRAIN_JUDGEMENT_restructured7.csv"
-ANNOT_PREAMBLE_DEV_PATH		=	"NER_DEV/NER_DEV/NER_DEV_PREAMBLE_restructured0.csv"
-ANNOT_JUDGEMENT_DEV_PATH	=	"NER_DEV/NER_DEV/NER_DEV_JUDGEMENT_restructured0.csv"
-FEATURES_TRAIN_PATH		=	"features_memory_train8.csv"
-FEATURES_DEV_PATH		=	"features_memory_dev8.csv"
+config_path	=	'TRAIN_DEV_MERGE_config.json'
 
-TEXT_BASE_PATH_OUT		=	"data/NER_MERGED_TEXT_BASE.csv"
-ANNOT_PREAMBLE_PATH_OUT		=	"data/NER_MERGED_PREAMBLE.csv"
-ANNOT_JUDGEMENT_PATH_OUT	=	"data/NER_MERGED_JUDGEMENT.csv"
-FEATURES_PATH_OUT		=	"features_memory_train8_MERGED.csv"
-
-
-SEP				=	"\t"
+try:
+	config_file	=	open(config_path,'rt',encoding='UTF-8')
+	config		=	json.load(config_file)
+	config_file.close()
+except IOError or OSError as e:
+	print(e)
+	print(f'Could not open {config_path}. Exiting.')
+	exit()
 
 ###
 
-df1	=	pd.read_csv(TEXT_BASE_TRAIN_PATH,sep=SEP,index_col=["id"],dtype={"text_id":"str"})
-df2	=	pd.read_csv(TEXT_BASE_DEV_PATH,sep=SEP,index_col=["id"],dtype={"text_id":"str"})
-df3	=	pd.concat([df1,df2])
-df3.to_csv(TEXT_BASE_PATH_OUT,sep=SEP)
-print(f'Wrote {TEXT_BASE_PATH_OUT}')
+def merge(in1,in2,out,update_id):
+	df1	=	pd.read_csv(in1,sep=config["SEP"],index_col=["id"],dtype={"text_id":"str"})
+	df2	=	pd.read_csv(in2,sep=config["SEP"],index_col=["id"],dtype={"text_id":"str"})
+	if update_id:
+		df2.index	+=	len(df1) #!
+	df3	=	pd.concat([df1,df2])
+	df3.to_csv(out,sep=config["SEP"])
+	print(f'Wrote {out}')
 
-df1	=	pd.read_csv(ANNOT_PREAMBLE_TRAIN_PATH,sep=SEP,index_col=["id"],dtype={"text_id":"str"})
-df2	=	pd.read_csv(ANNOT_PREAMBLE_DEV_PATH,sep=SEP,index_col=["id"],dtype={"text_id":"str"})
-df2.index	+=	len(df1)
-df3	=	pd.concat([df1,df2])
-df3.to_csv(ANNOT_PREAMBLE_PATH_OUT,sep=SEP)
-print(f'Wrote {ANNOT_PREAMBLE_PATH_OUT}')
+merge(config["TEXT_BASE_TRAIN_PATH_IN"],config["TEXT_BASE_DEV_PATH_IN"],config["TEXT_BASE_PATH_OUT"],False)
+merge(config["ANNOT_PREAMBLE_TRAIN_PATH_IN"],config["ANNOT_PREAMBLE_DEV_PATH_IN"],config["ANNOT_PREAMBLE_PATH_OUT"],True)
+merge(config["ANNOT_JUDGEMENT_TRAIN_PATH_IN"],config["ANNOT_JUDGEMENT_DEV_PATH_IN"],config["ANNOT_JUDGEMENT_PATH_OUT"],True)
+merge(config["FEATURES_TRAIN_PATH_IN"],config["FEATURES_DEV_PATH_IN"],config["FEATURES_PATH_OUT"],True)
 
-df1	=	pd.read_csv(ANNOT_JUDGEMENT_TRAIN_PATH,sep=SEP,index_col=["id"],dtype={"text_id":"str"})
-df2	=	pd.read_csv(ANNOT_JUDGEMENT_DEV_PATH,sep=SEP,index_col=["id"],dtype={"text_id":"str"})
-df2.index	+=	len(df1)
+'''
+df1	=	pd.read_csv(config["TEXT_BASE_TRAIN_PATH_IN"],sep=config["SEP"],index_col=["id"],dtype={"text_id":"str"})
+df2	=	pd.read_csv(config["TEXT_BASE_DEV_PATH_IN"],sep=config["SEP"],index_col=["id"],dtype={"text_id":"str"})
 df3	=	pd.concat([df1,df2])
-df3.to_csv(ANNOT_JUDGEMENT_PATH_OUT,sep=SEP)
-print(f'Wrote {ANNOT_JUDGEMENT_PATH_OUT}')
+df3.to_csv(config["TEXT_BASE_PATH_OUT"],sep=config["SEP"])
+print(f'Wrote {config["TEXT_BASE_PATH_OUT"]}')
 
-df1	=	pd.read_csv(FEATURES_TRAIN_PATH,sep=SEP,index_col=["id"],dtype={"text_id":"str"})
-df2	=	pd.read_csv(FEATURES_DEV_PATH,sep=SEP,index_col=["id"],dtype={"text_id":"str"})
-df2.index	+=	len(df1)
+df1	=	pd.read_csv(config["ANNOT_PREAMBLE_TRAIN_PATH_IN"],sep=config["SEP"],index_col=["id"],dtype={"text_id":"str"})
+df2	=	pd.read_csv(config["ANNOT_PREAMBLE_DEV_PATH_IN"],sep=config["SEP"],index_col=["id"],dtype={"text_id":"str"})
+df2.index	+=	len(df1) #!
 df3	=	pd.concat([df1,df2])
-df3.to_csv(FEATURES_PATH_OUT,sep=SEP)
-print(f'Wrote {FEATURES_PATH_OUT}')
+df3.to_csv(config["ANNOT_PREAMBLE_PATH_OUT"],sep=config["SEP"])
+print(f'Wrote {config["ANNOT_PREAMBLE_PATH_OUT"]}')
+
+df1	=	pd.read_csv(config["ANNOT_JUDGEMENT_TRAIN_PATH_IN"],sep=config["SEP"],index_col=["id"],dtype={"text_id":"str"})
+df2	=	pd.read_csv(config["ANNOT_JUDGEMENT_DEV_PATH_IN"],sep=config["SEP"],index_col=["id"],dtype={"text_id":"str"})
+df2.index	+=	len(df1) #!
+df3	=	pd.concat([df1,df2])
+df3.to_csv(config["ANNOT_JUDGEMENT_PATH_OUT"],sep=config["SEP"])
+print(f'Wrote {config["ANNOT_JUDGEMENT_PATH_OUT"]}')
+
+df1	=	pd.read_csv(config["FEATURES_TRAIN_PATH_IN"],sep=config["SEP"],index_col=["id"],dtype={"text_id":"str"})
+df2	=	pd.read_csv(config["FEATURES_DEV_PATH_IN"],sep=config["SEP"],index_col=["id"],dtype={"text_id":"str"})
+df2.index	+=	len(df1) #!
+df3	=	pd.concat([df1,df2])
+df3.to_csv(config["FEATURES_PATH_OUT"],sep=config["SEP"])
+print(f'Wrote {config["FEATURES_PATH_OUT"]}')
+'''
